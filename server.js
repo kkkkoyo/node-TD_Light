83fc2e7a253b98e138dbd6119cb61f0d70710870
kkkkoyo <ciku14005@g.nihon-u.ac.jp>
2018-02-21 18:26:31 +0900

    なおした

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 var osc = require('./node_modules/node-osc/lib');
 
-var oscServer = new osc.Server(1880, '192.168.0.0');
-oscServer.on('oscout2/chan1', function (msg) {
+var oscServer = new osc.Server(1880, '192.168.0.7');
+oscServer.on("message", function (msg) {
     console.log('Message:');
     console.log(msg);
 });
